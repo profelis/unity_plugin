c7f8b40c052b2924bf67b48323d1eaee5f9dd092
Dima Granetchi <system.grand@gmail.com>
2018-09-05 08:52:50 +0300

    fix clearall script

--- plugin/clearall.sh
+++ plugin/clearall.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-pushd build
-
 if [ "$(uname)" == "Darwin" ]; then
-rm -rf ios
-rm -rf mac
-rm -rf mac_editor
-rm -rf android
+rm -rf build/ios
+rm -rf build/mac
+rm -rf build/mac_editor
+rm -rf build/android
 else
-rm -rf windows
-rm -rf windows_editor
+rm -rf build/windows_x86
+rm -rf build/windows_x86_64
+rm -rf build/windows_editor_x86
+rm -rf build/windows_editor_x86_64
 fi
-
-popd
