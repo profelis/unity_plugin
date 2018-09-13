c6de61474aa78438d5f312c76fec1ab169e873bd
Dima Granetchi <system.grand@gmail.com>
2018-09-13 23:15:41 +0300

    vector tests colored output

--- unity_project/Assets/TestPlugin.cs
+++ unity_project/Assets/TestPlugin.cs
@@ -91,91 +91,105 @@ public class TestPlugin : MonoBehaviour
 
     [DllImport(PLUGIN_NAME)]
     static extern int swift_sum(int a, int b);
-#endif
-#endif
+#endif // SWIFT
+
+#endif // UNITY_EDITOR_OSX || UNITY_IOS || UNITY_STANDALONE_OSX
 
     void Start()
     {
-        Label.text = "Start test:\n";
-        Label.text += plugin_abs(1) + "\n";
-        Label.text += plugin_abs(-2) + "\n";
-        Label.text += plugin_sum(-4, 7) + "\n";
-        Label.text += plugin_sum(2, 2) + "\n";
+        var res = new StringBuilder();
+        res.AppendLine("Start test:");
+
+        var abs1 = plugin_abs(1);
+        res.AppendFormat("{0} {1}", abs1, Res(abs1 == 1));
+        var abs2 = plugin_abs(-2);
+        res.AppendFormat(" {0} {1}", abs2, Res(abs2 == 2));
+        var sum1 = plugin_sum(-4, 7);
+        res.AppendFormat(" {0} {1}\n", sum1, Res(sum1 == 3));
+        var sum2 = plugin_sum(2, 2);
+        res.AppendFormat("{0} {1}", sum2, Res(sum2 == 4));
 #if UNITY_EDITOR_OSX || UNITY_IOS || UNITY_STANDALONE_OSX
-        Label.text += objc_abs(5) + "\n";
-        Label.text += objc_sum(5, 1) + "\n";
+        var abs3 = objc_abs(5);
+        res.AppendFormat(" {0} {1}", abs3, Res(abs3 == 5));
+        var sum3 = objc_sum(5, 1);
+        res.AppendFormat(" {0} {1}\n", sum3, Res(sum3 == 6));
 #if SWIFT
-        Label.text += "Swift tests:\n";
+        res.AppendLine("Swift tests:");
         var swiftFailed = false;
         try
         {
-            Label.text += swift_abs(-7) + "\n";
-            Label.text += swift_sum(1, 7) + "\n";
-            Label.text += ("9 Test ended\n");
+            var abs4 = swift_abs(-7);
+            res.AppendFormat("{0} {1}", abs4, Res(abs4 == 7));
+            var sum4 = swift_sum(1, 7);
+            res.AppendFormat(" {0} {1}\n", sum4, Res(sum4 == 8));
         }
         catch (Exception e)
         {
             Debug.Log(e);
             swiftFailed = true;
-            Label.text += "swift test failed\n";
+            res.AppendLine("swift test failed");
         }
         if (swiftFailed)
-            Label.text += "7 Math Test ended\n";
-#else // SWIFT
-        Label.text += "7 Math Test ended\n";
+            res.AppendLine("Math Test ended");
 #endif // SWIFT
-
-#else
-        Label.text += "5 Math Test ended\n";
 #endif
+        res.AppendLine("Math Test ended");
 
-        Label.text += "\nStart string tests:\n";
+        res.AppendLine("\nStart string tests:");
 
-        Label.text += string.Format("equals {0} and not equals {1}\n", isEqualsString("foo", "foo"), isEqualsString("foo", "FOO"));
+        res.AppendFormat("equals {0} and not equals {1}\n", Res(isEqualsString("foo", "foo")), Res(!isEqualsString("foo", "FOO")));
 
         var dynamicString = getString();
-        Label.text += string.Format("getString returns: {0} correct: {1}\n", dynamicString, dynamicString.Equals("Dynamic string"));
+        res.AppendFormat("getString returns: {0} {1}\n", dynamicString, Res(dynamicString.Equals("Dynamic string")));
 
         var sb = new StringBuilder(200);
         getStringTo(sb, sb.Capacity);
-        Label.text += string.Format("StringBuilder contains {0} correct: {1}\n", sb, sb.ToString().Equals("Hello from c++"));
+        res.AppendFormat("StringBuilder contains {0} {1}\n", sb, Res(sb.ToString().Equals("Hello from c++")));
 
         var constString = MarshalingCommon.PtrToAnsiString(getConstString());
-        Label.text += string.Format("const string: {0} correct: {1}\n", constString, constString.Equals("some const string"));
+        res.AppendFormat("const string: {0} {1}\n", constString, Res(constString.Equals("some const string")));
 
-        Label.text += "String test ended\n";
+        res.AppendLine("String test ended");
 
-        Label.text += "Arrays test start:\n";
+        res.AppendLine("\nArrays test start:");
 
         var intArray = new int [3];
         WriteIntArray(intArray, intArray.Length);
         var valid = intArray[0] == 1 && intArray[1] == 2 && intArray[2] == 3;
-        Label.text += string.Format("int array:{0}, {1}, {2} correct: {3}\n", intArray[0], intArray[1], intArray[2], valid);
+        res.AppendFormat("int array:{0}, {1}, {2} {3}\n", intArray[0], intArray[1], intArray[2], Res(valid));
 
         var vector3Array = new Vector3 [3];
         var vector3ArrayHandle = GCHandle.Alloc(vector3Array, GCHandleType.Pinned);
         WriteVector3Array(vector3ArrayHandle.AddrOfPinnedObject(), vector3Array.Length);
-        Label.text += string.Format("vec3 array:{0}, {1}, {2} \n", vector3Array[0], vector3Array[1], vector3Array[2]);
+        res.AppendFormat("vec3 array[0]:{0} {1}\n", vector3Array[0], Res(Vector3.right.Equals(vector3Array[0])));
+        res.AppendFormat("vec3 array[1]:{0} {1}\n", vector3Array[1], Res(Vector3.up.Equals(vector3Array[1])));
+        res.AppendFormat("vec3 array[2]:{0} {1}\n", vector3Array[2], Res(Vector3.forward.Equals(vector3Array[2])));
         vector3ArrayHandle.Free();
 
-        Label.text += "Arrays test ended\n";
+        res.AppendLine("Arrays test ended");
 
-        Label.text += "Structs test start:\n";
+        res.AppendLine("\nStructs test start:");
         var len = PassVector3ByValue(Vector3.one);
-        Label.text += string.Format("Vector3.one len: {0} correct: {1}\n", len, Mathf.Approximately(len, Vector3.one.magnitude));
+        res.AppendFormat("Vector3.one len: {0} {1}\n", len, Res(Mathf.Approximately(len, Vector3.one.magnitude)));
 
         var vec = Vector3.one;
         var len2 = PassVector3ByRefIn(ref vec);
-        Label.text += string.Format("ref Vector3.one len: {0} correct: {1}\n", len2, Mathf.Approximately(len2, Vector3.one.magnitude));
+        res.AppendFormat("ref Vector3.one len: {0} {1}\n", len2, Res(Mathf.Approximately(len2, Vector3.one.magnitude)));
 
         var vec2 = Vector3.zero;
         PassVector3ByRefOut(out vec2);
-        Label.text += string.Format("out Vector3.zero : {0} correct: {1}\n", vec2, Mathf.Approximately(vec2.magnitude, Vector3.one.magnitude));
+        res.AppendFormat("out Vector3.zero->one : {0} {1}\n", vec2, Res(Vector3.one.Equals(vec2)));
 
         var vec3 = Vector3.one;
         PassVector3ByRefInOut(ref vec3);
-        Label.text += string.Format("int out Vector3.one : {0} correct: {1}\n", vec3, Mathf.Approximately(vec3.magnitude, (Vector3.one * 2).magnitude));
+        res.AppendFormat("int out Vector3.one * 2 : {0} {1}\n", vec3, Res((Vector3.one * 2).Equals(vec3)));
 
-        Label.text += "Structs test ended\n";
+        res.AppendLine("Structs test ended");
+        Label.text = res.ToString();
+    }
+
+    string Res(bool res)
+    {
+        return res ? "<color=#00ff00>Correct</color>" : "<color=#ff0000>Error</color>";
     }
 }
